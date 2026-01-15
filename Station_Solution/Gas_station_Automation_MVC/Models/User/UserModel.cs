6e543200eee4adfcca6d70408df63df7c2696ef4
Burak Furkan ERKEMEN <84676805+BurakErkemen@users.noreply.github.com>
2026-01-15 09:34:44 +0300

    Update

---
+++ Station_Solution/Gas_station_Automation_MVC/Models/User/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Gas_station_Automation_MVC.Models.User
+{
+    public class UserModel
+    {
+        public string User_Id { get; set; } = default!;
+        public string User_FullName { get; set; } = default!;
+        public string User_Password { get; set; } = default!;
+        public string User_Role { get; set; } = default!;
+        
+    }
+}
