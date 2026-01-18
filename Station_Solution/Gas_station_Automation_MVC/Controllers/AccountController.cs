95148f3c12f2ac5e86658ca690dc12fc854f94ef
Burak Furkan ERKEMEN <84676805+BurakErkemen@users.noreply.github.com>
2026-01-18 04:31:02 +0300

    update

--- Station_Solution/Gas_station_Automation_MVC/Controllers/AccountController.cs
+++ Station_Solution/Gas_station_Automation_MVC/Controllers/AccountController.cs
@@ -1,28 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Gas_station_Automation_MVC.Controllers
+namespace Gas_station_Automation_MVC.Controllers;
+
+public class AccountController : Controller
 {
-    public class AccountController : Controller
-    {
-        [HttpGet]
-        public IActionResult Login() => View();
-
-        [HttpPost]
-        public async Task<IActionResult> Login(string username, string password,bool rememberMe)
-        {
-            if(username != "test")
-            {
-                ViewBag.Error = "Kullanıcı adı ve şifre zorunludur.";
-                return View();
-            }
-
-            // Demo doğrulama (örnek): şifre "1234" ise kabul et
-            if (password != "1234")
-            {
-                ViewBag.Error = "Hatalı kullanıcı adı veya şifre.";
-                return View();
-            }
-            return RedirectToAction("Index", "Dashboard");
-        }
-    }
+    public IActionResult Login() => View();
 }
